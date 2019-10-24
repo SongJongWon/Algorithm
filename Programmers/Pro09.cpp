4d47ee98191be38f8be1c3fc4a0ff63ace500076
JongWon Song <foemzos@gmail.com>
2019-10-24 14:18:58 +0900

    2019/10/24

--- Programmers/Pro09.cpp
+++ Programmers/Pro09.cpp
@@ -29,7 +29,6 @@ int dfs(int y, int x, int tempVal) {
     return result;
 }
 
-// 전역 변수를 정의할 경우 함수 내에 초기화 코드를 꼭 작성해주세요.
 vector<int> solution(int m, int n, vector<vector<int>> picture) {
     vector<int> answer(2);
     int number_of_area = 0;
